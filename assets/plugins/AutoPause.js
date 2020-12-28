05e8e3b018e5a4c72622e1ebec8b9a452849a43f
SuichiM <MartiniRoqueEsteban@gmail.com>
2020-12-28 09:37:15 -0300

    using visibilitychange event to pause the video when the tab goes out or play when the tab goes in

--- assets/plugins/AutoPause.js
+++ assets/plugins/AutoPause.js
@@ -1,29 +1,38 @@
 class AutoPause {
-  constructor(){
-    this.THRESHOLD = 0.25
-    // con esto hacemos que siempre que se llame a _handleIntersection el this 
+  constructor() {
+    this.THRESHOLD = 0.25;
+    // con esto hacemos que siempre que se llame a _handleIntersection el this
     // esta instancia de la la clase
-    this._handleIntersection = this._handleIntersection.bind(this)
+    this._handleIntersection = this._handleIntersection.bind(this);
   }
 
   run(player) {
-    this.player = player 
+    this.player = player;
 
     const observer = new IntersectionObserver(this._handleIntersection, {
       threshold: this.THRESHOLD,
     });
 
-    observer.observe(this.player.media)
+    observer.observe(this.player.media);
+
+    document.addEventListener("visibilitychange", this._handleVisibilityChange);
   }
 
   _handleIntersection(entries) {
-    const [entry] = entries
+    const [entry] = entries;
 
-    const isVisible = entry.intersectionRatio >= this.THRESHOLD
+    const isVisible = entry.intersectionRatio >= this.THRESHOLD;
 
-    if(isVisible) this.player.play()
-    else
-    this.player.pause()
+    if (isVisible) this.player.play();
+    else this.player.pause();
+  }
+
+  _handleVisibilityChange = ()=> {
+    console.log(document.visibilityState)
+    const isVisible = document.visibilityState === 'visible';
+
+    if (isVisible) this.player.play();
+    else this.player.pause();
   }
 }
 
