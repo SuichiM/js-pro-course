a8a3b84fd1cd59e321a170ac93d7dfa23e7f8651
SuichiM <MartiniRoqueEsteban@gmail.com>
2020-12-30 09:00:06 -0300

    TS moar info

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -1,3 +1,9 @@
+/**
+ * 
+ *  TIPOS BASICOS 
+ * 
+ * */
+
 // Booleans
 let muted : boolean = true;
 muted = false
@@ -37,3 +43,62 @@ comodin = { type: 'Wildcard' };
 
 // Object
 let someObject: object = { type: 'Wildcard' };
+
+/**
+ * 
+ *  FUNCIONES 
+ * 
+ * */
+
+// tipando parametros y retorno 
+function add(a: number, b:number): number{
+  return a+b
+}
+
+// retornando funciones
+function createAdder(a: number): (number) => number{
+  return function(b:number){
+    return b + a
+  }
+}
+
+const addFour = createAdder(4)
+const fourPlus6 = addFour(6)
+
+// parametros opcionales
+
+function fullName(firstName:string, lastName?:string):string{
+  return `${firstName}${lastName}`
+}
+
+const richard = fullName('Richard')
+
+/**
+ * 
+ *  INTERFACES 
+ * 
+ * */
+
+ interface Rectangulo {
+   ancho: number;
+   alto: number;
+   color?: Color;
+ }
+
+ let rect: Rectangulo ={
+   alto:4,
+   ancho:6
+ }
+
+ function area(r:Rectangulo):number{
+   return r.alto*r.alto
+ }
+
+ const areaRect= area(rect)
+ console.log(areaRect)
+
+ rect.toString = function(){
+   return this.color ? `Un rectangulo de color ${this.color}`: `Un rectangulo`
+ }
+
+ console.log(rect.toString())
